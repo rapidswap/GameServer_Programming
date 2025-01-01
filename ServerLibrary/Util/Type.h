35c521a7b732985132ba39ddc6fc5aa2a30b618d
rapidswap <73596570+rapidswap@users.noreply.github.com>
2025-01-01 22:34:41 +0900

    Util
    랜덤 알고리즘, 서버시간

---
+++ ServerLibrary/Util/Type.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "pch.h"
+#include "framework.h"
+#include <atomic>
+
+// 타입 정의
+typedef UINT64                  object_t;
+typedef UINT64                  oid_t;
+typedef int32_t                 packet_size_t;
+
+typedef std::time_t             tick_t;
+typedef std::thread             thread_t;
+typedef std::thread::id         threadId_t;
+
+typedef std::recursive_mutex    lock_t;
+
+typedef std::string             str_t;
+typedef std::wstring            wstr_t;
+
+// 크기 정의
+#define SIZE_8          8
+#define SIZE_64         64
+#define SIZE_128        128
+#define SIZE_256        256
+#define SIZE_1024       1024
+#define SIZE_4096       4096
+#define SIZE_8192       8192
+
+#define DB_PARAM_SIZE   8192
+#define SOCKET_BUF_SIZE 1024*10
+
+// for xml
+typedef TiXmlDocument       xml_t;
+typedef TiXmlElemet         xmlNode_t;
+typedef TiXmlHandle         xmlHandle_t;
--- ServerLibrary/framework.h
+++ ServerLibrary/framework.h
@@ -1,3 +1,9 @@
 #pragma once
 
+//#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include "ServerLibrary.h"
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
